a79f0b7e77d31eaaa3c287453d5a2aa225cd7335
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2023-04-07 12:56:38 +0300

    feat(app): enable Material Design 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,16 +9,29 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Recipe App")),
-        body: const Center(child: Text("Hi, cooklang!")),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => DynamicColorBuilder(
+        builder: (light, dark) => MaterialApp(
+          title: 'Recipes',
+          theme: ThemeData(
+            colorScheme: light ??
+                ColorScheme.fromSeed(
+                  seedColor: const Color(0xffe15a29),
+                  brightness: Brightness.light,
+                ),
+            useMaterial3: true,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: dark ??
+                ColorScheme.fromSeed(
+                  seedColor: const Color(0xffe15a29),
+                  brightness: Brightness.dark,
+                ),
+            useMaterial3: true,
+          ),
+          home: Scaffold(
+            appBar: AppBar(title: const Text("Recipe App")),
+            body: const Center(child: Text("Hi, cooklang!")),
+          ),
+        ),
+      );
 }
